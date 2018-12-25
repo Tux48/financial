e519d992687912291cb01fe139797cd73ce8b4b8
Administrator <Administrator@XTZ-20181222XRG>
2018-12-25 22:18:56 +0800

    增加db、log相关工具类

---
+++ common/src/com/financial/common/db/MongoDBConnection.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3.7
+#-*- coding: utf-8 -*-
+'''
+Created on 2018-12-25
+
+com.financial.common.db.MongoDBConnection -- shortdesc
+
+com.financial.common.db.MongoDBConnection is a description
+
+It defines classes_and_methods
+
+@author: Administrator
+
+@version: 0.1
+
+@copyright:  2018 organization_name. All rights reserved.
+
+@deffield    updated: Updated
+'''
---
+++ common/src/com/financial/common/db/MySqlDBConnection.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3.7
+#-*- coding: utf-8 -*-
+'''
+Created on 2018-12-25
+
+com.financial.common.db.MySqlDBConnection -- shortdesc
+
+com.financial.common.db.MySqlDBConnection is a description
+
+It defines classes_and_methods
+
+@author: Administrator
+
+@version: 0.1
+
+@copyright:  2018 organization_name. All rights reserved.
+
+@deffield    updated: Updated
+'''
