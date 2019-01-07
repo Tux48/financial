60340d0cc2b7b7b60ce3266eaac423cfcc7964ad
Tux48 <Tux48@Tux48-PC>
2019-01-07 16:15:20 +0800

    基本的K线模块

---
+++ KLine/src/com/financial/kline/cfg/KLineConfig.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3.7
+#-*- coding: utf-8 -*-
+'''
+Created on 2019Äê1ÔÂ7ÈÕ
+
+com.financial.kline.cfg.KLineConfig -- shortdesc
+
+com.financial.kline.cfg.KLineConfig is a description
+
+It defines classes_and_methods
+
+@author: Tux48
+
+@version: 0.1
+
+@copyright:  2019 organization_name. All rights reserved.
+
+@deffield    updated: Updated
+'''
